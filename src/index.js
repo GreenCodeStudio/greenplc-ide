cb33e74fdaa1ebc801dcc9eb0b233de4188cb70e
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2026-07-23 05:01:44 +0200

    .

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import './style/indes.scss'
 
 console.log('Hello, world!')
 document.body.append(new LayoutComponent())
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.register('/sw.js').then(() => {
+      console.log('Service worker registered.');
+    }).catch((err) => {
+      console.warn('Service worker registration failed:', err);
+    });
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 export default {
     entry: './src/index.js',
 
@@ -20,7 +20,18 @@ export default {
         ],
     },
 
-    plugins: [new HtmlWebpackPlugin()],
+    plugins: [
+        new HtmlWebpackPlugin({
+            templateContent: ({htmlWebpackPlugin}) => `<!doctype html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="manifest" href="/manifest.json"><link rel="icon" href="/icons/icon.svg" type="image/svg+xml"><meta name="theme-color" content="#2b7a0b"></head><body><div id="root"></div></body></html>`
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'manifest.json', to: '' },
+                { from: 'icons', to: 'icons' },
+                { from: 'sw.js', to: '' }
+            ]
+        })
+    ],
 
     resolve: {
         extensions: ['.js', '.jsx', '.scss', '.css'],
